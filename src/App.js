b7393c638d008ec01f6021b58dc11e6b8870d49c
rene <rene.cardozo@encora.com>
2023-02-10 15:47:26 -0400

    Chat in React with Cometchat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import ChatGroup from './components/ChatGroup';
+import chat from './lib/chat';
+import {useEffect} from 'react';
+
 
 function App() {
+
+  useEffect(() => {
+    chat.init();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={ <Navigate to='/login'/>}/>
+        <Route exact path='/login' element={<Login />} />
+        <Route path='/chat' element={<ChatGroup />}/>
+      </Routes>
     </div>
   );
 }
